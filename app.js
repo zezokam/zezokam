787755d147d3b606447a6512da2fe197e2c2f634
zezokam <125836314+zezokam@users.noreply.github.com>
2023-02-21 01:46:38 +0400

    Update app.js

--- app.js
+++ app.js
@@ -7,19 +7,50 @@ const newListButton = document.getElementById("new-list");
 const titleElement = document.getElementsByTagName("title")[0];
 const h1Element = document.getElementsByTagName("h1")[0];
 
-// Load saved list from local storage
-let savedList = JSON.parse(localStorage.getItem("taskList"));
-if (savedList) {
-  for (let i = 0; i < savedList.length; i++) {
-    let count = i + 1;
-    let input = document.createElement("input");
-    input.type = "text";
-    input.value = savedList[i];
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(count));
-    li.appendChild(input);
-    taskList.appendChild(li);
+// Load saved list from server
+fetch("/tasks.json")
+  .then(response => response.json())
+  .then(data => {
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+        let count = i + 1;
+        let input = document.createElement("input");
+        input.type = "text";
+        input.value = data[i];
+        let li = document.createElement("li");
+        li.dir = "rtl";
+        li.appendChild(document.createTextNode(count + ". "));
+        li.appendChild(input);
+        taskList.appendChild(li);
+      }
+    }
+  })
+  .catch(error => {
+    console.error("Failed to load tasks from server:", error);
+  });
+
+// Save task list to server
+function saveTaskList() {
+  let tasks = [];
+  let listItems = taskList.getElementsByTagName("li");
+  for (let i = 0; i < listItems.length; i++) {
+    let input = listItems[i].getElementsByTagName("input")[0];
+    tasks.push(input.value);
   }
+
+  fetch("/tasks.json", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(tasks)
+  })
+    .then(response => {
+      console.log("Task list saved successfully:", response);
+    })
+    .catch(error => {
+      console.error("Failed to save task list:", error);
+    });
 }
 
 // Add task to list
@@ -29,31 +60,35 @@ function addTask() {
   input.type = "text";
   input.placeholder = "مهمة جديدة";
   let li = document.createElement("li");
-  li.appendChild(document.createTextNode(count));
+  li.dir = "rtl";
+  li.appendChild(document.createTextNode(count + ". "));
   li.appendChild(input);
   taskList.appendChild(li);
 }
 
-// Save list to local storage
-function saveList() {
-  let tasks = [];
-  let taskElements = taskList.getElementsByTagName("li");
-  for (let i = 0; i < taskElements.length; i++) {
-    let input = taskElements[i].getElementsByTagName("input")[0];
-    if (input.value) {
-      tasks.push(input.value);
-    }
+// Edit list title
+function editTitle() {
+  let newTitle = prompt("أدخل العنوان الجديد:", titleElement.innerHTML);
+  if (newTitle) {
+    titleElement.innerHTML = newTitle;
+    h1Element.innerHTML = newTitle;
   }
-  localStorage.setItem("taskList", JSON.stringify(tasks));
 }
 
-// Edit title
-function editTitle() {
-  let newTitle = prompt("Enter new title:");
-  if (newTitle) {
-    titleElement.innerText = newTitle;
-    h1Element.innerText = newTitle;
+// Clear task list
+function clearList() {
+  if (confirm("هل أنت متأكد من أنك تريد حذف كل المهام؟")) {
+    taskList.innerHTML = "";
   }
+}
+
+// Create new task list
+function newTaskList() {
+  if (confirm("هل أنت متأكد من أنك تريد إنشاء قائمة مهام جديدة؟")) {
+    taskList.innerHTML = "";
+    editTitle();
+  }
+
 }
 
 // Clear list
