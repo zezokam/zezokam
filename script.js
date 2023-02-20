5d7ffdd8ad09f6911e3ea6f624e1d98d6148636a
zezokam <125836314+zezokam@users.noreply.github.com>
2023-02-20 06:02:34 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-const guestList = document.getElementById("guest-list");
-const addGuestButton = document.getElementById("add-guest-btn");
-const savePdfButton = document.getElementById("save-pdf-btn");
-const listTitleInput = document.getElementById("list-title");
+let addListBtn = document.getElementById('add-list-btn');
+let guestList = document.getElementById('guest-list');
 
-// Function to add a new guest to the list
-function addGuest() {
-  const guestName = prompt("Enter the guest name:");
-  if (guestName) {
-    const newGuest = document.createElement("li");
-    newGuest.textContent = guestName;
-    guestList.appendChild(newGuest);
-  }
-}
-
-// Function to save the guest list as a PDF
-function savePdf() {
-  const doc = new jsPDF();
-  doc.text(listTitleInput.value, 10, 10);
-  const guests = guestList.getElementsByTagName("li");
-  let y = 20;
-  for (let i = 0; i < guests.length; i++) {
-    doc.text(`${i + 1}. ${guests[i].textContent}`, 10, y);
-    y += 10;
-  }
-  doc.save("guest-list.pdf");
-}
-
-addGuestButton.addEventListener("click", addGuest);
-savePdfButton.addEventListener("click", savePdf);
+addListBtn.addEventListener('click', function() {
+	let newGuestList = document.createElement('ol');
+	newGuestList.id = 'guest-list';
+	guestList.after(newGuestList);
+	guestList = newGuestList;
+});
