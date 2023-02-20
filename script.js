b4e34ca55c45c425ac446feff02ca80bae126d95
zezokam <125836314+zezokam@users.noreply.github.com>
2023-02-20 14:16:01 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let taskList = document.getElementById("task-list");
 let title = "قائمة المهام";
 
+// Check if there's a saved list and load it
+if(localStorage.getItem('tasks')) {
+  taskList.innerHTML = localStorage.getItem('tasks');
+}
+
 function addTask() {
   let count = taskList.getElementsByTagName("li").length + 1;
   let input = document.createElement("input");
@@ -10,6 +15,9 @@ function addTask() {
   li.appendChild(document.createTextNode(count));
   li.appendChild(input);
   taskList.appendChild(li);
+
+  // Save the updated list
+  saveList();
 }
 
 function editTitle() {
@@ -20,11 +28,25 @@ function editTitle() {
 
 function clearList() {
   taskList.innerHTML = "";
+
+  // Save the cleared list
+  saveList();
 }
 
 function createPDF() {
-  // Code to generate and download PDF file
-  alert("تم تصدير القائمة بنجاح");
+  // Get the task list element and create a new jsPDF instance
+  let doc = new jsPDF();
+  let list = document.getElementById("task-list");
+
+  // Loop through each list item and add it to the PDF
+  for(let i = 0; i < list.children.length; i++) {
+    let listItem = list.children[i];
+    let task = listItem.getElementsByTagName("input")[0].value;
+    doc.text((i + 1) + ". " + task, 10, 10 + (i * 10));
+  }
+
+  // Save and download the PDF file
+  doc.save("task-list.pdf");
 }
 
 function newList() {
@@ -33,4 +55,12 @@ function newList() {
   title = "قائمة المهام";
   document.getElementsByTagName("title")[0].innerHTML = title;
   document.getElementsByTagName("h1")[0].innerHTML = title;
+
+  // Save the new list
+  saveList();
+}
+
+function saveList() {
+  // Save the current list in localStorage
+  localStorage.setItem('tasks', taskList.innerHTML);
 }
