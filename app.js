5d718b8204a1a527d692183512153366cc207739
zezokam <125836314+zezokam@users.noreply.github.com>
2023-02-20 23:38:34 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// Get DOM elements
+const taskList = document.getElementById("task-list");
+const addTaskButton = document.getElementById("add-task");
+const editTitleButton = document.getElementById("edit-title");
+const clearListButton = document.getElementById("clear-list");
+const newListButton = document.getElementById("new-list");
+const titleElement = document.getElementsByTagName("title")[0];
+const h1Element = document.getElementsByTagName("h1")[0];
+
+// Load saved list from local storage
+let savedList = JSON.parse(localStorage.getItem("taskList"));
+if (savedList) {
+  for (let i = 0; i < savedList.length; i++) {
+    let count = i + 1;
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = savedList[i];
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(count));
+    li.appendChild(input);
+    taskList.appendChild(li);
+  }
+}
+
+// Add task to list
+function addTask() {
+  let count = taskList.getElementsByTagName("li").length + 1;
+  let input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "مه
