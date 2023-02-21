a361b48e842401d32e885b9f872f9df3552ebdd3
zezokam <125836314+zezokam@users.noreply.github.com>
2023-02-22 01:50:29 +0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('generate-btn').addEventListener('click', function() {
-  var qrText = document.getElementById('qr-text').value;
-  if(qrText !== '') {
-    new QRCode(document.getElementById('qrcode'), qrText);
-  }
-});
