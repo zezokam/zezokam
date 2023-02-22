6f4e74db1f3c107c5179b2c54b50d09233d19e02
zezokam <125836314+zezokam@users.noreply.github.com>
2023-02-22 05:59:30 +0400

    png to jepg

--- qrcode.js
+++ qrcode.js
@@ -274,7 +274,7 @@ var QRCode;
 		return Drawing;
 	})() : (function () { // Drawing in Canvas
 		function _onMakeImage() {
-			this._elImage.src = this._elCanvas.toDataURL("image/png");
+			this._elImage.src = this._elCanvas.toDataURL("image/jpeg");
 			this._elImage.style.display = "block";
 			this._elCanvas.style.display = "none";			
 		}
